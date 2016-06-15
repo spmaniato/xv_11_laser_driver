6aa6e13240f57e2250a6953ecf4e195fd97799ac
Spyros Maniatopoulos <spmaniato@gmail.com>
2016-06-15 15:06:59 -0500

    Correct check for good packet. Clean up code.

--- src/xv11_laser.cpp
+++ src/xv11_laser.cpp
@@ -35,6 +35,7 @@
 #include <xv_11_laser_driver/xv11_laser.h>
 
 namespace xv_11_laser_driver {
+
   XV11Laser::XV11Laser(const std::string& port, uint32_t baud_rate, uint32_t firmware, boost::asio::io_service& io): port_(port),
   baud_rate_(baud_rate), firmware_(firmware), shutting_down_(false), serial_(io, port_) {
     serial_.set_option(boost::asio::serial_port_base::baud_rate(baud_rate_));
@@ -106,134 +107,131 @@ namespace xv_11_laser_driver {
     // This is for the newer driver that outputs packets 4 pings at a time
     } else if (firmware_ == 2) {
 
-      const uint8_t HEADER_BYTE      = 0xFA;
+      const float ONE_DEGREE = (2.0*M_PI/360.0);
+
+      scan->angle_min = 0.0;
+      scan->angle_max = 2.0 * M_PI - ONE_DEGREE; // No double-count
+      scan->angle_increment = ONE_DEGREE;
+      scan->range_min = 0.15;
+      scan->range_max = 5.0;
+      scan->ranges.resize(360);
+      scan->intensities.resize(360);
+
+      const uint8_t HEADER_BYTE = 0xFA;
       const uint8_t FIRST_INDEX_BYTE = 0xA0;
 
-      boost::array<uint8_t, 1980> raw_bytes;
-      boost::array<uint8_t, 22> packet;
+      boost::array<uint8_t, 2100> raw_bytes; // If no errors, it would be 1980
+
+      int angle; // The index of the scan.ranges array. Ranges from 0 to 359
       uint8_t packet_index; // Second byte of a packet. Ranges from 0 to 89
-      uint8_t good_packets = 0;
+      bool good_packet;
+      uint8_t good_packets = 0; // Number of good packets. Ranges from 0 to 90.
       uint32_t motor_speed = 0;
       rpms = 0;
-      int index;
 
       while (!shutting_down_ && !got_scan) {
 
-          // Wait until first data sync of frame: 0xFA, 0xA0
-          boost::asio::read(serial_, boost::asio::buffer(&raw_bytes[start_count],1));
-          if(start_count == 0) {
-              if(raw_bytes[start_count] == HEADER_BYTE) {
-                  start_count = 1;
-              }
-          } else if(start_count == 1) {
-              if(raw_bytes[start_count] == FIRST_INDEX_BYTE) {
-                  start_count = 0;
+        // Wait until first data sync of frame, i.e., 0xFA, 0xA0
+        boost::asio::read(serial_, boost::asio::buffer(&raw_bytes[start_count], 1));
 
-                  // Now that entire start sequence has been found, read in the rest of the message
-                  got_scan = true;
+        if (start_count == 0) {
+          if (raw_bytes[start_count] == HEADER_BYTE) {
+            start_count = 1;
+          }
+        } else if (start_count == 1) {
+          if (raw_bytes[start_count] == FIRST_INDEX_BYTE) {
+            start_count = 0;
 
-                  boost::asio::read(serial_, boost::asio::buffer(&raw_bytes[2], 1978));
+            // Now that the entire start sequence has been found,
+            // start reading packets until the laser scan is complete
+            got_scan = true;
 
-                  const float ONE_DEGREE = (2.0*M_PI/360.0);
+            boost::asio::read(serial_, boost::asio::buffer(&raw_bytes[2], 1978));
 
-                  scan->angle_min = 0.0;
-                  scan->angle_max = 2.0 * M_PI - ONE_DEGREE; // No double-count
-                  scan->angle_increment = ONE_DEGREE;
-                  scan->range_min = 0.15;
-                  scan->range_max = 5.0;
-                  scan->ranges.resize(360);
-                  scan->intensities.resize(360);
+            uint16_t i = 0; // Iterates over the raw byte stream
 
-                  uint16_t i = 0; // Iterates over the raw byte stream
+            while (!shutting_down_ && (i + 22 < raw_bytes.size())) {
 
-                  //read data in sets of 4
-                  while (!raw_bytes.empty()) {
+              packet_index = (raw_bytes[i+1] - FIRST_INDEX_BYTE);
 
-                      packet_index = (raw_bytes[i+1] - FIRST_INDEX_BYTE);
+              if (raw_bytes[i] == HEADER_BYTE && packet_index >= 0
+                                                && packet_index < 90) {
 
-                      if (raw_bytes[i] == HEADER_BYTE && packet_index >= 0
-                                                      && packet_index < 90) {
+                // Check whether this seems to be a proper packet of length 22
+                good_packet = true; // Start optimistically
 
-                        // Assemble the contents of this packet
-                        packet[0] = HEADER_BYTE;
-                        packet[1] = raw_bytes[i+1];
+                for (uint16_t k = 2; k < 22; k++) {
 
-                        uint16_t k; // Iterates over a single packet's bytes
-
-                        for (k = 2; k < 22; k++) {
-
-                            if (raw_bytes[i+k] == HEADER_BYTE) {
-                                i = i + k;
-                                break; // Unexpected header byte. Skip!
-                            }
-                            packet[k] = raw_bytes[i+k];
-                        }
-
-                        if (k == 21) {
-                        // TODO: CRC checksum too before declaring good packet
-
-                            good_packets++;
-
-                            // Accumulate count for average time increment of scan
-                            motor_speed += (raw_bytes[i+3] << 8) + raw_bytes[i+2];
-                            rpms = (raw_bytes[i+3]<<8 | raw_bytes[i+2]) / 64;
-
-                            // Iterate over the 4 measurements of this packet
-                            for (uint16_t j = i+4; j < i+20; j=j+4) {
-
-                                // Calculate the bearing angle (index of ranges)
-                                index = (4 * packet_index) + (j-4-i)/4;
-
-                                // Four bytes per measurement
-                                uint8_t byte0 = raw_bytes[j];
-                                uint8_t byte1 = raw_bytes[j+1];
-                                uint8_t byte2 = raw_bytes[j+2];
-                                uint8_t byte3 = raw_bytes[j+3];
-
-                                // First two bits of byte1 are status flags
-                                // uint8_t flag1 = (byte1 & 0x80) >> 7;  // No return/max range/too low of reflectivity
-                                // uint8_t flag2 = (byte1 & 0x40) >> 6;  // Object too close, possible poor reading due to proximity kicks in at < 0.6m
-
-                                // Remaining bits are the range in mm
-                                uint16_t range = ((byte1 & 0x3F)<< 8) + byte0;
-
-                                // Last two bytes represent the uncertainty or intensity, might also be pixel area of target...
-                                uint16_t intensity = (byte3 << 8) + byte2;
-
-                                scan->ranges[index] = range / 1000.0;
-                                scan->intensities[index] = intensity;
-                            }
-
-                            i = i + k + 1; // Set index to start of next packet
-                        }
-
-                    // } else if (raw_bytes[i] == 0xFA && raw_bytes[i+1] == FIRST_INDEX_BYTE) {
-                    //      std::cout << "\n<-- Unexpected start of new revolution! (0xFA, 0xA0) -->\n\n";
-                    //      i++;
-                    //
-                    // } else if (i != 0 && raw_bytes[i+1] == FIRST_INDEX_BYTE) {
-                    //     std::cout << "\nUnexpected start byte (0xA0) in packet " << (i/22 + 1) << "\n\n";
-                    //     i++;
-
-                    } else {
-                        // std::cout << std::showbase      // Show the 0x hex prefix
-                        //           << std::internal      // Fill between the prefix and the number
-                        //           << std::setfill('0'); // Fill with 0s if less than 4 digits
-                        //
-                        // std::cout << "Unexpected start (" << std::hex << std::setw(4)
-                        //           << static_cast<int>(raw_bytes[i]) << ") "
-                        //           << "or index (" << std::hex << std::setw(4)
-                        //           << static_cast<int>(raw_bytes[i+1]) << ") "
-                        //           << "in packet " << std::dec << static_cast<int>(i/22 + 1) << "\n";
-                        i++;
-                    }
+                  // If there is an unexpected header byte, skip this packet
+                  if (raw_bytes[i+k] == HEADER_BYTE) {
+                    good_packet = false;
+                    i = i + k;
+                    break;
+                  }
                 }
 
-                std::cout << "<-- Good packets for this revolution = "
-                          << static_cast<int>(good_packets) << " / 90 -->\n";
+                if (good_packet) {
+                  // TODO: CRC checksum too before declaring good packet
 
-                scan->time_increment = motor_speed/good_packets/1e8;
+                  good_packets++;
+                  // std::cout << "Good packet starting at i = " << i << "\n";
+
+                  // Accumulate count for average time increment of scan
+                  motor_speed += (raw_bytes[i+3] << 8) + raw_bytes[i+2];
+                  rpms = (raw_bytes[i+3]<<8 | raw_bytes[i+2]) / 64;
+
+                  // Iterate over the 4 measurements of this packet
+                  for (uint16_t j = i+4; j < i+20; j=j+4) {
+
+                    // Calculate the bearing angle (index of ranges)
+                    angle = (4 * packet_index) + (j-4-i)/4;
+
+                    // Four bytes per measurement
+                    uint8_t byte0 = raw_bytes[j];
+                    uint8_t byte1 = raw_bytes[j+1];
+                    uint8_t byte2 = raw_bytes[j+2];
+                    uint8_t byte3 = raw_bytes[j+3];
+
+                    // The first two bits of byte1 are status flags
+                    // No return/max range/too low of reflectivity
+                    uint8_t flag1 = (byte1 & 0x80) >> 7;
+                    // Object too close, possible poor reading due to proximity kicks in at < 0.6m
+                    uint8_t flag2 = (byte1 & 0x40) >> 6;
+
+                    // Remaining bits are the range in mm
+                    uint16_t range = ((byte1 & 0x3F)<< 8) + byte0;
+
+                    // Last two bytes represent the uncertainty or
+                    // intensity, might also be pixel area of target
+                    uint16_t intensity = (byte3 << 8) + byte2;
+
+                    scan->ranges[angle] = range / 1000.0;
+                    scan->intensities[angle] = intensity;
+                  }
+
+                  if (angle == 359) {
+                    break; // Laser scan message is full
+                  } else {
+                    i = i + 22; // Set index to start of next packet
+                  }
+
+                // } else {
+                //   std::cout << "Bad packet starting at i = " << i - k
+                //             << " Found unexpected header byte at i = " << i
+                //             << "\n";
+
+                } // End of packet length (and eventually CRC) check
+
+              } else {
+                i++;
+              } // End of start of packet check
             }
+
+            // std::cout << "<-- Good packets for this revolution = "
+            //           << static_cast<int>(good_packets) << " / 90 -->\n";
+
+            scan->time_increment = motor_speed / good_packets / 1e8;
+          }
         }
       }
     }
